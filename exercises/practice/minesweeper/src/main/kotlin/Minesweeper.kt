ff1e00932a7caa73bd9ad93c04c0dc03578de4bd
Tarnic <nico.tarquini23@gmail.com>
2022-03-08 10:32:20 +0100

    minesweeper

--- exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
+++ exercises/practice/minesweeper/src/main/kotlin/Minesweeper.kt
@@ -1,8 +1,26 @@
-data class MinesweeperBoard(val todo: Nothing) {
-
-    // TODO: Implement proper constructor
+data class MinesweeperBoard(val inputBoard: List<String>) {
 
     fun withNumbers(): List<String> {
-        TODO("Implement this function to complete the task")
+        // TODO("Implement this function to complete the task")
+
+        return inputBoard.mapIndexed { row, str -> 
+            str.mapIndexed { col, el -> when(el) {
+                    ' ' -> checkAdjacentMines(row, col)
+                    else -> el
+                } 
+            }.joinToString("")
+        }
+    }
+
+    fun checkAdjacentMines(row: Int, col: Int): Char {
+        val cols = Math.max(row-1, 0)..Math.min(row+1, inputBoard.size-1)
+        
+        val count = cols.flatMap { i ->
+            (Math.max(col-1, 0)..Math.min(col+1, inputBoard[i].length-1)).map { j -> 
+                if(inputBoard[i][j] == '*') 1 else 0
+            }
+        }.sum()
+
+        return if (count > 0) (count + 48).toChar() else ' '
     }
 }
