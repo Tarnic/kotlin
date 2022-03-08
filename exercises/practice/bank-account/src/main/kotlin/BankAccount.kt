cd0b359248c04e8e8dd37369d377ccaec22379e5
Tarnic <nico.tarquini23@gmail.com>
2022-03-08 11:15:10 +0100

    bank-account done

--- exercises/practice/bank-account/src/main/kotlin/BankAccount.kt
+++ exercises/practice/bank-account/src/main/kotlin/BankAccount.kt
@@ -1,11 +1,18 @@
 class BankAccount {
     // TODO: implement read access to 'balance'
+    private var open = true;
+    var balance: Long = 0                   // we use custom getter and private setter to check if the account is open before setting or returning the balance 
+        get() { check(open); return field}  // check(bool) throws IllegalStateException if bool is false
+        private set                         // we can't modify the balance value but using the adjustBalance function
 
     fun adjustBalance(amount: Long){
-        TODO("Implement the function to complete the task")
+        check(open)
+        synchronized(this) {                // synchronized function implements a lock to avoid multiple access in the critical section
+            balance += amount
+        }
     }
 
     fun close() {
-        TODO("Implement the function to complete the task")
+        open = false;
     }
 }
